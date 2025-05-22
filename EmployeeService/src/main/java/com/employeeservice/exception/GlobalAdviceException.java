b7f75ce0a8c00d39f082b7925969fd4a9d93b7a7
Kousiganesh-0302 <144134898+Kousiganesh-0302@users.noreply.github.com>
2025-05-22 19:30:44 +0530

    Update GlobalAdviceException.java

--- EmployeeService/src/main/java/com/employeeservice/exception/GlobalAdviceException.java
+++ EmployeeService/src/main/java/com/employeeservice/exception/GlobalAdviceException.java
@@ -133,17 +133,17 @@ public class GlobalAdviceException {
         return new ResponseEntity<>(body, HttpStatus.CONFLICT);
     }
 
-    // 500 - All Other Exceptions
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", "Internal Server Error");
-        body.put("message", "An unexpected error occurred");
-        body.put("path", request.getDescription(false).replace("uri=", ""));
+    // // 500 - All Other Exceptions
+    // @ExceptionHandler(Exception.class)
+    // public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+    //     Map<String, Object> body = new LinkedHashMap<>();
+    //     body.put("timestamp", LocalDateTime.now());
+    //     body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+    //     body.put("error", "Internal Server Error");
+    //     body.put("message", "An unexpected error occurred");
+    //     body.put("path", request.getDescription(false).replace("uri=", ""));
 
-        log.error("Unexpected error: ", ex);
-        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    //     log.error("Unexpected error: ", ex);
+    //     return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    // }
 }
